d75f00418973c7e0bc08a13a08c6fdcf74ee6fbb
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2021-04-23 15:49:55 +0200

    1

---
+++ js/header.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("nav");
+
+window.onscroll = function() {
+
+    const top = window.scrollY;
+    
+    if (top >= 50) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+}
